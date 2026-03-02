8d5a2aa0202620c7e04a271e6a7d14a6c45874c3
Shaela Bello <slbello@uncg.edu>
2026-03-02 10:40:05 -0500

    chore(lint): add ESLint v9 flat config parity

---
+++ integrations/eslint.config.cjs
@@ -0,0 +1,40 @@
+const tsParser = require("@typescript-eslint/parser");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+
+module.exports = [
+  {
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "**/*.test.ts",
+      "**/*.spec.ts",
+      "**/ui-smoke/**",
+      "**/playwright.config.ts",
+    ],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          caughtErrors: "none",
+        },
+      ],
+      "no-console": "off",
+    },
+  },
+];
